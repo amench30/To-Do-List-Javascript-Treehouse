8d2cf633469cfaab03ad745d26967af6670e1b22
amench30 <mencher@gmail.com>
2015-01-12 20:33:50 -0800

    completed my plan for my to do list

---
+++ js/app.js
@@ -0,0 +1,46 @@
+//problem: user interaction doesn't provice desired results.
+//soultion: add interactivity so that the user can manage daily tasks.
+
+
+//Add a new task
+  //When athe botton is pressed
+  //Create a new list item with the text from #new-task:
+    //input(Checkbox)
+    //lable
+    //input(text)
+    //button.edit
+    //button.delete
+    //each elements, needs modified and appended
+
+
+//Edit an exsiting task
+  //When the Edit button is pressed
+    //if the class of thr parent is .editMode
+      //switch from .editMode
+      //lable text become the input's value
+    //else
+      //switch to .editMode
+      //input value becomes the label's text
+
+    //toggle .editMode on the parent
+
+//Delete an exsisting task
+  //When the delete button is pressed
+    //remove the parent list item from the ul
+
+//mark the task as complete
+  //When the checkbox is checked
+    //append the task list item to the #completed-tasks
+
+//Mark a task as incomplete
+  //When the checkbox is unchecked
+    //Appent the task list item the the #incomplete-tasks
+
+
+//delete an exsiting task
+
+
+//Mark a task as complete
+
+
+//Mark a task incomplete
