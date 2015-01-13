1964d0711adb86932e95af919b68af0d97e02762
amench30 <mencher@gmail.com>
2015-01-12 21:01:32 -0800

    added functions and global scope var's

--- js/app.js
+++ js/app.js
@@ -1,46 +1,49 @@
-//problem: user interaction doesn't provice desired results.
-//soultion: add interactivity so that the user can manage daily tasks.
+//problem: user interaction doesn't provide desired results.
+//solution: add interactivity so that the user can manage daily tasks.
 
+var taskInput = document.getElementById("new-task"); //new-task
+var addButton = document.getElementsByTagName("button")[0]; //First button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-task"); //completed-tasks
 
 //Add a new task
-  //When athe botton is pressed
+var addTask = function() {
+  //When the button is pressed
   //Create a new list item with the text from #new-task:
     //input(Checkbox)
-    //lable
+    //label
     //input(text)
     //button.edit
     //button.delete
     //each elements, needs modified and appended
+}
 
 
-//Edit an exsiting task
+//Edit an existing task
+var editTsk = function () {
   //When the Edit button is pressed
-    //if the class of thr parent is .editMode
+    //if the class of the parent is .editMode
       //switch from .editMode
-      //lable text become the input's value
+      //label text become the input's value
     //else
       //switch to .editMode
       //input value becomes the label's text
 
     //toggle .editMode on the parent
+}
 
-//Delete an exsisting task
+//Delete an existing task
+var deleteTask = function() {
   //When the delete button is pressed
     //remove the parent list item from the ul
-
+}
 //mark the task as complete
+var taskCompleted = function () {
   //When the checkbox is checked
     //append the task list item to the #completed-tasks
-
+}
 //Mark a task as incomplete
+var taskIncomplete = function () {
   //When the checkbox is unchecked
-    //Appent the task list item the the #incomplete-tasks
-
-
-//delete an exsiting task
-
-
-//Mark a task as complete
-
-
-//Mark a task incomplete
+    //Append the task list item the the #incomplete-tasks
+}
